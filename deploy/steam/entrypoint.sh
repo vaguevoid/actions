9fd0ccc79f2b4ef7f375d8c4b199a1804d6168b8
Jake Gordon <jake@void.dev>
2024-06-22 11:10:41 -0700

    break out separate deploy/steam/login image for performing a manual login to regenerate the config.vdf

--- deploy/steam/entrypoint.sh
+++ deploy/steam/entrypoint.sh
@@ -4,28 +4,6 @@ STEAM_HOME=${STEAM_HOME:-$HOME/.steam/steam}
 STEAM_CONFIG_DIR=${STEAM_HOME}/config
 STEAM_CONFIG_FILE=${STEAM_CONFIG_DIR}/config.vdf
 
-if [ $1="login" ]; then
-  if [[ -z $2 ]]; then
-    echo "username required"
-    exit 1
-  fi
-  OUTPUT=${OUTPUT:-/output}
-  if [ ! -d ${OUTPUT} ]; then
-    echo "must mount ${OUTPUT} volume. Try -v .:${OUTPUT}"
-    exit 1
-  fi
-  steamcmd +login $2 +quit
-  if [ $? -eq 0 ]; then
-    mkdir -p /out
-    cat "${STEAM_CONFIG_FILE}" | base64 > "${OUTPUT}/config.vdf"
-    echo "config.vdf saved"
-    exit 0
-  else
-    echo "invalid password"
-    exit 1
-  fi
-fi
-
 if [[ -z "${CONFIG_VDF}" ]]; then
   echo "\$CONFIG_VDF required"
   exit 1
