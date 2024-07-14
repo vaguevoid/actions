244e124041f98521687a8a9c816456b42fa97270
Jake Gordon <jake@void.dev>
2024-07-14 10:55:51 -0700

    detect 404 status code and report a more suitable error message indicating org/game might be incorrect

--- deploy/web/entrypoint.sh
+++ deploy/web/entrypoint.sh
@@ -43,11 +43,18 @@ fi
 tar -czf ${bundle} -C ${path} .
 
 echo "Uploading ${bundle} to ${endpoint}"
-OUTPUT=$(curl -s -X POST --fail-with-body --connect-timeout 180 --max-time 180 -H "X-Deploy-Password: ${password}" -H "Authorization: Bearer ${token}" --data-binary "@${bundle}" ${endpoint})
-if [ $? -ne 0 ]; then
-  echo $OUTPUT
+OUTPUT_FILE=$(mktemp)
+STATUS_CODE=$(curl -s -X POST -o $OUTPUT_FILE -w "%{http_code}" --connect-timeout 180 --max-time 180 -H "X-Deploy-Password: ${password}" -H "Authorization: Bearer ${token}" --data-binary "@${bundle}" ${endpoint} 2>/dev/null)
+
+if [[ $STATUS_CODE == "404" ]]; then
+  echo "endpoint ${endpoint} not found, are the organization and game correct?"
+  exit 1
+fi
+
+if [[ $STATUS_CODE != "200" ]]; then
+  echo "STATUS: $STATUS_CODE"
   echo "Sorry, upload failed, please try again in a few minutes or contact support@void.dev"
   exit 1
 fi
 
-echo $OUTPUT
+cat $OUTPUT_FILE
