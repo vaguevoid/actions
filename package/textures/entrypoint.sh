8e96bf4352dde8016efcf1bf428e1d7512dfa37e
Jake Gordon <jake@void.dev>
2024-09-05 15:30:35 -0700

    update share/on/steam to use new image-compressor and expose DXT vs KTX option

--- package/textures/entrypoint.sh
+++ package/textures/entrypoint.sh
@@ -2,7 +2,8 @@
 
 path=${path:-release/web}
 ignore=${ignore:-favicon.png}
+format=${format:-DXT}
 
 set -x
 
-/usr/local/bin/png_compressor -d ${path} -i ${ignore}
+/usr/local/bin/image_compressor -d ${path} -i ${ignore} --delete-original-images -c ${format}
