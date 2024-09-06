a415cda501b5ccf58a7f07d0ba962658f6328da8
Jake Gordon <jake@void.dev>
2024-09-06 16:09:58 -0700

    allow image-compressor -t threads to be configurable, and default to 4 for standard GitHub runner

--- package/textures/entrypoint.sh
+++ package/textures/entrypoint.sh
@@ -3,7 +3,10 @@
 path=${path:-release/web}
 ignore=${ignore:-favicon.png}
 format=${format:-DXT}
+threads=${threads:-4}
 
 set -x
 
-/usr/local/bin/image_compressor -d ${path} -i ${ignore} --delete-original-images -c ${format}
+date
+/usr/local/bin/image_compressor -d ${path} -i ${ignore} --delete-original-images -c ${format} -t ${threads}
+date
