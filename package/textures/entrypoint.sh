6d527ab4066e28157e045abb997846cb030c664e
Jake Gordon <jake@void.dev>
2024-09-24 13:31:19 -0700

    ensure package/textures action exits with failure code if image_compressor fails

--- package/textures/entrypoint.sh
+++ package/textures/entrypoint.sh
@@ -9,4 +9,11 @@ set -x
 
 date
 /usr/local/bin/image_compressor -d ${path} -i ${ignore} --delete-original-images -c ${format} -t ${threads}
+result=$?
 date
+
+if [ $result -ne 0 ]; then
+  echo "Image Compressor Failed"
+  echo "Exit code: $result"
+  exit $result
+fi
