57bbce3fec4741f5133c9f0b7f48124cf43046d4
Jake Gordon <jake@void.dev>
2025-05-27 11:09:06 -0700

    switch from 'curl --data-binary' to 'curl -T' to stream file instead of reading it into memory

--- deploy/web/entrypoint.sh
+++ deploy/web/entrypoint.sh
@@ -44,7 +44,7 @@ tar -czf ${bundle} -C ${path} .
 
 echo "Uploading ${bundle} to ${endpoint}"
 OUTPUT_FILE=$(mktemp)
-STATUS_CODE=$(curl -s -X POST -o $OUTPUT_FILE -w "%{http_code}" --connect-timeout 180 --max-time 300 -H "X-Deploy-Label: ${label}" -H "X-Deploy-Password: ${password}" -H "X-Deploy-Pinned: ${pinned}" -H "Authorization: Bearer ${token}" --data-binary "@${bundle}" ${endpoint} 2>/dev/null)
+STATUS_CODE=$(curl -s -X POST -o $OUTPUT_FILE -w "%{http_code}" --connect-timeout 180 --max-time 300 -H "X-Deploy-Label: ${label}" -H "X-Deploy-Password: ${password}" -H "X-Deploy-Pinned: ${pinned}" -H "Authorization: Bearer ${token}" -T "${bundle}" ${endpoint} 2>/dev/null)
 
 if [[ $STATUS_CODE == "404" ]]; then
   echo "endpoint ${endpoint} not found, are the organization and game correct?"
@@ -54,6 +54,7 @@ fi
 if [[ $STATUS_CODE != "200" ]]; then
   echo "STATUS: $STATUS_CODE"
   echo "Sorry, upload failed, please try again in a few minutes or contact support@void.dev"
+  cat $OUTPUT_FILE
   exit 1
 fi
 
